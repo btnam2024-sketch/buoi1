d5582ff205cf132040fc230f43fe4c307b44bde9
btnam2024-sketch <btnam2024@gmail.com>
2025-09-16 10:34:46 +0700

    Add files via upload

---
+++ DPM225445_BuiThanhNam_MyProject1_Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Singleton s1 = Singleton.Instance();
+        Singleton s2 = Singleton.Instance();
+
+        if (s1 == s2)
+        {
+            Console.WriteLine("Same instance");
+        }
+
+        Console.ReadKey(); // giữ console không bị đóng ngay
+    }
+}
+
+public class Singleton
+{
+    private static Singleton instance;
+    protected Singleton() { }
+
+    public static Singleton Instance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
