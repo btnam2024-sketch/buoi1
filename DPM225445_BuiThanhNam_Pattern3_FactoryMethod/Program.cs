379e7502aa3d82c5668e75b523a526e2bcafbbee
btnam2024-sketch <btnam2024@gmail.com>
2025-09-30 10:30:37 +0700

    Add files via upload

---
+++ DPM225445_BuiThanhNam_Pattern3_FactoryMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MainApp
+{
+
+    static void Main()
+    {
+        // Mảng creators
+
+        Creator[] creators = new Creator[2];
+
+        creators[0] = new ConcreteCreatorA();
+        creators[1] = new ConcreteCreatorB();
+
+        // Lặp lại creators và tạo products
+
+        foreach (Creator creator in creators)
+        {
+            Product product = creator.FactoryMethod();
+            Console.WriteLine("Created {0}",
+              product.GetType().Name);
+        }
+
+        // Chờ người dùng
+
+        Console.ReadKey();
+    }
+}
